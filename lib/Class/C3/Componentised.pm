e1950a66337b4572634e4b99cd57a702398889c2
Peter Rabbitson <ribasushi@cpan.org>
2011-02-24 18:01:00 +0000

    Stupid regression - the primitive $isa{$_} does not take in consideration the ISA of added parents

--- lib/Class/C3/Componentised.pm
+++ lib/Class/C3/Componentised.pm
@@ -188,13 +188,10 @@ sub inject_base {
   my $class = shift;
   my $target = shift;
 
-  my %isa = map { $_ => 1 } ($target, @{mro::get_linear_isa($target)} );
-
   for (reverse @_) {
     no strict 'refs';
-    unless ($isa{$_}++) {
-      unshift ( @{"${target}::ISA"}, $_ );
-    }
+    unshift ( @{"${target}::ISA"}, $_ )
+      unless ($target eq $_ || $target->isa($_));
   }
 
   mro::set_mro($target, 'c3');
