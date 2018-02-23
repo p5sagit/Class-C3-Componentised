1030fc6468ed92aaafe768ce2734dbade8886cfe
Graham Knop <haarg@haarg.org>
2018-02-23 14:41:11 +0100

    Bumping version to 1.001002

--- lib/Class/C3/Componentised.pm
+++ lib/Class/C3/Componentised.pm
@@ -46,7 +46,7 @@ use MRO::Compat;
 use Carp ();
 use List::Util ();
 
-our $VERSION = '1.001_001';
+our $VERSION = '1.001002';
 $VERSION =~ tr/_//d;
 
 my $invalid_class = qr/(?: \b:\b | \:{3,} | \:\:$ )/x;
